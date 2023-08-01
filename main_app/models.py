678995d0a9e8b31d81494ec8a4b924b30386bac1
janicavc <crisostomo_janica@yahoo.com>
2023-08-01 11:41:06 -0700

    added profile and pet models

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
 class Place(models.Model):
     name = models.CharField(max_length=50)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    city = models.CharField(max_length=50)
+    contributions  = models.IntegerField(default=0)
+
+    def get_absolute_url(self):
+        return reverse('profile_detail', kwargs={'profile_id': self.id})
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    breed = models.CharField(max_length=50)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    
+
+
