e055b22e8e4daf0dc9770a52ad64398fef272de1
B-Selin <selin@bozkurt.ca>
2023-08-02 17:30:12 -0600

    Add/show pet pictures viq AWS

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Pet, Profile, Place, Review
+from .models import Pet, Profile, Place, Review, Photo
 
 # Register your models here.
 admin.site.register(Place)
 admin.site.register(Pet)
 admin.site.register(Profile)
 admin.site.register(Review)
+admin.site.register(Photo)
+
