0e11413d47b6161293c10cd304ac35bb7bf14c41
mxrgxr <mgxrcias@gmail.com>
2023-08-06 11:34:03 -0700

    merge amenities feature

---
+++ main_app/amenities.py
@@ -0,0 +1,99 @@
+AMENITY_CHOICES = [
+    ("airport", (
+        ("potty_location", "Potty Location"),
+        ("water", "Water"),
+    )),
+    ("amusement_park", (
+        ("potty_location", "Potty Location"),
+        ("water", "Water"),
+        ("dog_friendly_rides", "Dog-Friendly Rides"),
+    )),
+    ("aquarium", (
+        ("potty_location", "Potty Location"),
+        ("water", "Water"),
+    )),
+    ("art_gallery", (
+        ("potty_location", "Potty Location"), 
+        ("water", "Water"),
+        ("pet_friendly_exhibits", "Pet-Friendly Exhibits"),
+    )),
+    ("bakery", (
+        ("dog_treats", "Dog Treats"),
+        ("outdoor_seating", "Outdoor Seating"),
+    )),
+    ("cafe", (
+        ("dog_treats", "Dog Treats"),
+        ("outdoor_seating", "Outdoor Seating"),
+        ("peaceful_walking_area", "Peaceful Walking Area"),
+        ("dog_treats", "Dog Treats"),
+        ("outdoor_seating", "Outdoor Seating"),
+    )),
+    ("park", (
+        ("off_leash_area", "Off-Leash Area"),
+        ("peaceful_walking_area", "Peaceful Walking Area"),
+        ("waste_bags", "Waste Bags"),
+        ("water", "Water"),
+    )),
+    ("beach", (
+        ("off_leash_area", "Off-Leash Area"),
+        ("water", "Water"),
+        ("waste_bags", "Waste Bags"),
+    )),
+    ("campground", (
+        ("off_leash_area", "Off-Leash Area"),
+        ("peaceful_walking_area", "Peaceful Walking Area"),
+        ("waste_bags", "Waste Bags"),
+    )),
+    ("pet_store", (
+        ("dog_treats", "Dog Treats"),
+        ("pet_supplies", "Pet Supplies"),
+    )),
+    ("vet_clinic", (
+        ("dog_friendly_waiting_area", "Dog-Friendly Waiting Area"),
+        ("pet_healthcare", "Pet Healthcare"),
+    )),
+    ("grooming_salon", (
+        ("dog_grooming_services", "Dog Grooming Services"),
+    )),
+        ("shopping_mall", (
+        ("pet_friendly_shops", "Pet-Friendly Shops"),
+        ("dog_friendly_stores", "Dog-Friendly Stores"),
+    )),
+    ("supermarket", (
+        ("dog_friendly_shopping", "Dog-Friendly Shopping"),
+        ("pet_supplies", "Pet Supplies"),
+    )),
+    ("parking", (
+        ("pet_friendly_parking", "Pet-Friendly Parking"),
+    )),
+    ("pet_store", (
+        ("dog_treats", "Dog Treats"),
+        ("pet_supplies", "Pet Supplies"),
+        ("pet_friendly_staff", "Pet-Friendly Staff"),
+    )),
+    ("pharmacy", (
+        ("pet_medications", "Pet Medications"),
+    )),
+    ("restaurant", (
+        ("dog_friendly_seating", "Dog-Friendly Seating"),
+        ("water_bowl", "Water Bowl for Pets"),
+        ("dog_treats", "Dog Treats"),
+        ("outdoor_seating", "Outdoor Seating"),
+    )),
+    ("veterinary_care", (
+        ("emergency_care", "Emergency Pet Care"),
+        ("pet_healthcare", "Pet Healthcare"),
+        ("potty_location", "Potty Location"),
+        ("water", "Water"),
+        ("dog_treats", "Dog Treats"),
+    )),
+    ("university", (
+        ("pet_friendly_campus", "Pet-Friendly Campus"),
+        ("potty_location", "Potty Location"),
+        ("water", "Water"),
+    )),
+    ("zoo", (
+        ("pet_friendly_zoo", "Pet-Friendly Zoo"),
+        ("potty_location", "Potty Location"),
+    )),
+]
